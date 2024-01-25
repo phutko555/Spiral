e4466582fa638ee457966a35284af377aa183544
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-25 13:17:03 +0000

    Update Spiral.java

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,31 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[][] result = new int[rows][columns];
+        int num = 1;
+        int top = 0, bottom = rows - 1, left = 0, right = columns - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result[top][i] = num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = num++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        return result;
     }
 }
